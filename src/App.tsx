e6bb2ced857c38dfb5e2e716180275e323af9878
Mykhailo Odynets <mykhailo.odynets.023@pslib.cz>
2025-03-09 15:50:32 +0100

    connecting fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import { useState } from "react";
-import "./styles/App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="header">
+        <span className="icon-">cheveron-up</span>
+        <p>Traiding Simulator</p>
+      </header>
     </>
   );
 }
